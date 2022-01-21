c412f870ea09d940a36e33e5bde8ee8a50c5d508
Dirk Arnez <smalldirkalex@gmail.com>
2022-01-21 18:39:02 +0800

    - update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ while true; do
         echo "$FILE exists.\n"
         s=$(cat $FILE); [[ -z "$s" ]] 
         echo $s
+        break;
     else
         echo "$FILE does not exist.\n"
         echo "this is a line" > $FILE
